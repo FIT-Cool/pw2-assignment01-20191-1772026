fb7fc470c67f8fdfa83a5226f375be115a7327f2
1772026 <it1772026@student.it.maranatha.edu>
2019-10-12 23:08:26 +0700

    update repo

--- PW2T02/entity/User.php
+++ PW2T02/entity/User.php
@@ -3,29 +3,37 @@
 
 class User
 {
-    private $user,$password;/**
- * @return mixed
- */
-public function getUser()
-{
-    return $this->user;
-}/**
- * @param mixed $username
- */
-public function setUser($user)
-{
-    $this->user = $user;
-}/**
- * @return mixed
- */
-public function getPassword()
-{
-    return $this->password;
-}/**
- * @param mixed $password
- */
-public function setPassword($password)
-{
-    $this->password = $password;
-}
+    private $user, $password;
+
+    /**
+     * @return mixed
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param mixed $username
+     */
+    public function setUser($user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
 }
